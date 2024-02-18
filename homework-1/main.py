c7ae2112e26e74d148c8fa1057104e84e2835420
Egor Shmelev <shmelevyegor25@gmail.com>
2024-02-18 19:08:03 -0300

    changes to main.py

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,34 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+
+import psycopg2
+import csv
+
+conn = psycopg2.connect(
+    host='localhost',
+    database='north',
+    user='postgres',
+    password='irongrom2'
+)
+try:
+    with conn:
+        with conn.cursor() as cur:
+            with open('north_data/employees_data.csv') as file:
+                reader = csv.reader(file)
+                next(reader)
+                for row in reader:
+                    cur.execute('INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)', row)
+
+            with open('north_data/customers_data.csv') as file:
+                reader = csv.reader(file)
+                next(reader)
+                for row in reader:
+                    cur.execute('INSERT INTO customers VALUES (%s, %s, %s)', row)
+
+            with open('north_data/orders_data.csv') as file:
+                reader = csv.reader(file)
+                next(reader)
+                for row in reader:
+                    cur.execute('INSERT INTO orders VALUES (%s, %s, %s, %s, %s)', row)
+
+finally:
+    conn.close()
